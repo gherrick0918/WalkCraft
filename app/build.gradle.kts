15fc860c056de34b71acbb4a30b7013b284e58f4
gherrick0918 <gherrick0918@gmail.com>
2025-10-24 19:05:42 -0400

    Updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.walkcraft.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 29
-        versionName = "1.0.29"
+        versionCode = 32
+        versionName = "1.0.32"
 
         // Add the test instrumentation runner
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
